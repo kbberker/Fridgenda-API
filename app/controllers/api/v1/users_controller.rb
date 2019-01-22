255763709eb6a87e6f2557d0c2ea1c6a196b2734
kbberker <kbbuk90@gmail.com>
2019-01-22 13:19:37 +0000

    Added rest-client to Gemfile.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,14 +11,14 @@ class Api::V1::UsersController < ApplicationController
     if @user
       render json: @user
     else
-      render json: {error: "User not found."}, status: 400
+      render json: {error: "User not found."}, status: 404
     end
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @username
+      render json: @user
     else
       render json: {error: "Unable to create user."}, status: 400
   end
@@ -31,6 +31,10 @@ class Api::V1::UsersController < ApplicationController
     else
       render json: {error: "User not found."}, status: 400
     end
+  end
+
+  def signin
+    @user = User.find_by(user_params)
   end
 
   private
