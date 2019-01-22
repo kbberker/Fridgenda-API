b9ac2232e3cc62a217c2ddaf8c69f9ddd0b3e400
kbberker <kbbuk90@gmail.com>
2019-01-22 17:46:55 +0000

    Forced seeding of data using a copy of API data as a string.

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -7,7 +7,16 @@ class Api::V1::RecipesController < ApplicationController
 
   def recipes
     recipes = Recipe.get_recipes
-    render json: recipes
+    # render json: recipes
+    recipes_json = JSON.parse(recipes)
+    recipes_json["recipes"].each do |recipe|
+      new_recipe = Recipe.new("name": recipe["title"])    
+      if new_recipe
+        new_recipe.save
+      else
+        puts `#{recipe["title"]} with id:#{recipe["recipe_id"]} could not be saved`
+      end
+    end
   end
 
 end
